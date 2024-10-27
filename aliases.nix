783c32f01eebc5cfde7a49642a8c76f2f4e9aa63
Supermarcel10 <marcelbarlik@gmail.com>
2024-10-27 20:52:48 +0000

    - Added alias to edit configuration.nix directly.

--- aliases.nix
+++ aliases.nix
@@ -12,5 +12,8 @@
     nixos = "cd /etc/nixos/";
     desktop = "cd ~/Desktop/";
     downloads = "cd ~/Downloads/";
+
+    # NixOS Edit
+    nixos-cfg = "cd /etc/nixos/ && nvim configuration.nix";
   };
 }
