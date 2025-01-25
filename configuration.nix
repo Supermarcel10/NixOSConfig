0feb09316437733489a69d69877699098e15d7b7
Supermarcel10 <marcelbarlik@gmail.com>
2025-01-25 14:33:17 +0000

    - Updated DNS nameservers.

--- configuration.nix
+++ configuration.nix
@@ -36,6 +36,12 @@
 
   # Networking
   networking.networkmanager.enable = true;
+  networking.nameservers = [
+		"1.1.1.1" # Cloudflare (IPv4)
+		"2606:4700:4700::1111"  # Cloudflare (IPv6)
+		"1.0.0.1" # Cloudflare Backup (IPv4)
+		"2606:4700:4700::1001" # Cloudflare Backup (IPv6)
+	];
   # networking.wireless.enable = true;
 
   # Configure network proxy if necessary
