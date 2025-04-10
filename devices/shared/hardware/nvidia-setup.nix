858a4665d33fc9d8aacf7edb6f1778cdee67ff43
Marcel <47798787+Supermarcel10@users.noreply.github.com>
2025-04-10 10:34:50 +0100

    - Fixed black screen suspend on Nvidia drivers

--- devices/shared/hardware/nvidia-setup.nix
+++ devices/shared/hardware/nvidia-setup.nix
@@ -15,4 +15,8 @@
 		nvidiaSettings = true;
 		package = config.boot.kernelPackages.nvidiaPackages.stable;
 	};
+
+	# Fix suspend black screen
+	# https://discourse.nixos.org/t/black-screen-after-suspend-hibernate-with-nvidia/54341/21
+  systemd.services.systemd-suspend.environment.SYSTEMD_SLEEP_FREEZE_USER_SESSIONS = "false";
 }
