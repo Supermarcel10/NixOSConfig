98c1de74e234e094597f685bcd44e888b519ea52
Marcel <47798787+Supermarcel10@users.noreply.github.com>
2025-04-11 11:54:48 +0100

    - Altered firefox

--- profiles/generic.nix
+++ profiles/generic.nix
@@ -1,8 +1,56 @@
 { config, pkgs, agenix, ... }:
 
+let
+        lock-false = {
+                Value = false;
+                Status = "locked";
+        };
+        lock-true = {
+                Value = true;
+                Status = "locked";
+        };
+in
 {
-	# TODO: Separate browser and create configurations
-	programs.firefox.enable = true;
+  # TODO: Separate browser and create configurations
+        # https://discourse.nixos.org/t/declare-firefox-extensions-and-settings/36265
+  programs.firefox = {
+                enable = true;
+                package = pkgs.firefox-wayland;
+
+                /* ---- POLICIES ---- */
+    # Check about:policies#documentation for options.
+                policies = {
+                        DisableTelemetry = true;
+                        DisableFirefoxStudies = true;
+                        EnableTrackingProtection = {
+                                Value = true;
+                                Locked = true;
+                                Cryptomining = true;
+                                Fingerprinting = true;
+      };
+      DisablePocket = true;
+                        DisableFirefoxScreenshots = true;
+                        DontCheckDefaultBrowser = true;
+                        DisplayBookmarksToolbar = "always";
+
+                        /* ---- PREFERENCES ---- */
+      # Check about:config for options.
+                        Preferences = {
+                                "extensions.pocket.enabled" = lock-false;
+                                "extensions.screenshots.disabled" = lock-true;
+                                "browser.newtabpage.activity-stream.feeds.section.topstories" = lock-false;
+                                "browser.newtabpage.activity-stream.feeds.snippets" = lock-false;
+                                "browser.newtabpage.activity-stream.section.highlights.includePocket" = lock-false;
+                                "browser.newtabpage.activity-stream.section.highlights.includeBookmarks" = lock-false;
+                                "browser.newtabpage.activity-stream.section.highlights.includeDownloads" = lock-false;
+                                "browser.newtabpage.activity-stream.section.highlights.includeVisited" = lock-false;
+                                "browser.newtabpage.activity-stream.showSponsored" = lock-false;
+                                "browser.newtabpage.activity-stream.system.showSponsored" = lock-false;
+                                "browser.newtabpage.activity-stream.showSponsoredTopSites" = lock-false;
+                                "browser.tabs.groups.enabled" = lock-true;
+                        };
+                };
+        };
 
 	nixpkgs.config.allowUnfree = true;
 
