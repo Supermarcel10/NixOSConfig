4a5f5f69117c082f2270c371780397c903f16fc9
Supermarcel10 <47798787+Supermarcel10@users.noreply.github.com>
2025-10-22 00:47:27 +0100

    - Added AMD GPU configuration

--- devices/marcel-pc/configuration.nix
+++ devices/marcel-pc/configuration.nix
@@ -4,7 +4,7 @@
   imports =
     [
       ./hardware-configuration.nix
-			./../shared/hardware/nvidia-gpu.nix
+			./../shared/hardware/amd-gpu.nix
 
       # TODO: Find a neater way to have neater shared configs
       ./../shared/configuration.nix
---
+++ devices/shared/hardware/amd-gpu.nix
@@ -0,0 +1,15 @@
+{ config, lib, pkgs, ... }:
+
+{
+	boot.initrd.kernelModules = ["amdgpu"];
+	services.xserver.videoDrivers = ["amdgpu"];
+
+	hardware.graphics = {
+		enable = true;
+		enable32Bit = true;
+		extraPackages = with pkgs; [
+			rocmPackages.clr.icd
+			clinfo
+		];
+	};
+}
