64b687c402781b16dd2811eaa22dc03d543044d7
marcel <47798787+Supermarcel10@users.noreply.github.com>
2026-02-18 00:55:19 +0000

    - Setup scanning setup

--- devices/shared/printing.nix
+++ devices/shared/printing.nix
@@ -1,6 +1,7 @@
 { config, pkgs, ... }:
 
 {
+	# Printing
   services.printing = {
     enable = true;
     drivers = with pkgs; [ cups-filters cups-browsed ];
@@ -11,4 +12,14 @@
     nssmdns4 = true;
     openFirewall = true;
   };
+
+	# Scanning
+  hardware.sane.enable = true;
+  hardware.sane.extraBackends = [ pkgs.sane-airscan ];
+  hardware.sane.disabledDefaultBackends = [ "escl" ];
+
+	# Software
+	environment.systemPackages = with pkgs; [
+		naps2 # Scanning Utility
+	];
 }
