c922cb9ccdbafd908c724ebe3c600d82d1a09033
marcel <47798787+Supermarcel10@users.noreply.github.com>
2026-02-18 00:57:00 +0000

    - Add Cosmic DE

---
+++ devices/shared/desktop_environments/cosmic/environment.nix
@@ -0,0 +1,12 @@
+{ config, pkgs, ... }:
+
+{
+	# COSMIC Login Manager
+  services.displayManager.cosmic-greeter.enable = true;
+
+	# COSMIC Desktop Environment
+  services.desktopManager.cosmic.enable = true;
+
+	# System76 Scheduler (Slight Performance Uplift)
+	services.system76-scheduler.enable = true;
+}
