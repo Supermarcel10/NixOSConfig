84806b7ee9a8700f9053ad5dcdab7ef6e3de8739
marcel <47798787+Supermarcel10@users.noreply.github.com>
2026-02-18 00:58:57 +0000

    - Update AMD GPU setup

--- devices/shared/hardware/amd-gpu.nix
+++ devices/shared/hardware/amd-gpu.nix
@@ -2,14 +2,15 @@
 
 {
 	boot.initrd.kernelModules = ["amdgpu"];
+	boot.kernelParams = [
+    "amdgpu.ppfeaturemask=0xffffffff"
+		"amdgpu.gpu_recovery=1"
+  ];
 	services.xserver.videoDrivers = ["amdgpu"];
 
+  hardware.amdgpu.overdrive.enable = true;
 	hardware.graphics = {
 		enable = true;
 		enable32Bit = true;
-		extraPackages = with pkgs; [
-			rocmPackages.clr.icd
-			clinfo
-		];
 	};
 }
