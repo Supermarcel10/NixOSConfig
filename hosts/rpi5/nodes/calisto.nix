69bc31ce55cdea91cd6447b91904ecc75d1ecfa5
marcel <47798787+Supermarcel10@users.noreply.github.com>
2026-04-15 00:25:12 +0100

    - Set specific local IPs

--- hosts/rpi5/nodes/calisto.nix
+++ hosts/rpi5/nodes/calisto.nix
@@ -3,5 +3,14 @@
 {
   networking.hostName = "calisto";
 
+  networking.interfaces.end0 = {
+    ipv4.addresses = [
+      { address = "192.168.1.50"; prefixLength = 24; }
+    ];
+    ipv6.addresses = [
+      { address = "fd00::1:0:0:50"; prefixLength = 64; }
+    ];
+  };
+
   age.secrets.hostKey.file = paths.secrets + /calisto-host-key.age;
 }
--- hosts/rpi5/nodes/ganymede.nix
+++ hosts/rpi5/nodes/ganymede.nix
@@ -3,5 +3,14 @@
 {
   networking.hostName = "ganymede";
 
+  networking.interfaces.end0 = {
+    ipv4.addresses = [
+      { address = "192.168.1.52"; prefixLength = 24; }
+    ];
+    ipv6.addresses = [
+      { address = "fd00::1:0:0:52"; prefixLength = 64; }
+    ];
+  };
+
   age.secrets.hostKey.file = paths.secrets + /ganymede-host-key.age;
 }
